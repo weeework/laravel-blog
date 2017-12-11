cb01d3725ab68f5713419a90812f6823dc796d11
Hakim <hakimsembiring@outlook.co.id>
2017-12-11 22:32:56 +0700

    crud using query builder

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,37 @@ Route::get('/blog', 'PostController@index');
 // }]);
 
 Route::resource('post', 'PostController');
+
+
+Route::get('/insert', function () {
+    // DB::insert('insert into posts(title, body, user_id) values (?,?,?)', ['Belajar PHP dengan Laravel', 'Laravel the best framework', '1']);
+    $data = [
+        'title' => 'Disini isian title',
+        'body' => 'Isian Body untuk table posts',
+        'user_id' => 2
+    ];
+    DB::table('posts')->insert($data);
+    echo "Data berhasil ditambah";
+});
+
+Route::get('/read', function () {
+    $query = DB::select('select * from posts where id = ?', [1]);
+    $query = DB::table('posts')->where('id', 1)->first();
+    return var_dump($query);
+});
+
+Route::get('/update', function () {
+    // $updated = DB::update('update posts set title = "UPdate field title" where id = ?', [1]);
+    $data = [
+        'title' => 'Isian Title',
+        'body' => 'Isian Body baru'
+    ];
+    $updated = DB::table('posts')->where('id', 1)->update($data);
+    return $updated;
+});
+
+Route::get('/delete', function () {
+    // $delete = DB::delete('delete from posts where id = ?', [1]);
+    $delete = DB::table('posts')->where('id', 2)->delete();
+    return $delete;
+});
